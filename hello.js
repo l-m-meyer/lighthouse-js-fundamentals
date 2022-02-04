a5fa63f9e0d4ee70a1a141b91076062cdee2669f
Lisa Meyer <lisamermey@gmail.com>
2022-02-04 12:19:57 -0500

    demonstrate printing to the console vs returnto perform the same function

--- hello.js
+++ hello.js
@@ -1,5 +1,14 @@
-const sayHello = function (name) {
+// function prints Hello, <name> to the console
+const sayHelloToConsole = function (name) {
   console.log('Hello,', name);
 }
 
-sayHello('John');
+sayHelloToConsole('John');
+
+// function returns Hello, <name>
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
